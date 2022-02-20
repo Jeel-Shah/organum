4f74d84858d8994b31bed36471575abca05ecd2d
Jeel Shah <jeelshah96@gmail.com>
2022-02-20 16:14:04 -0500

    Edited the project url to include "seylerius"

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.seriouslyseylerius/organum "0.2.0"
   :description "Org-mode parser in clojure"
-  :url "https://github.com/gmorpheme/organum"
+  :url "https://github.com/seylerius/organum"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
